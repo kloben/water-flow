08a91c52d698d1bdceb1e6a931ef61b8745e4028
davidvalgamedios <dvdvalero89@gmail.com>
2019-09-18 10:17:44 +0200

    Allow basic drawing (no water flow)

--- src/cell.ts
+++ src/cell.ts
@@ -1,8 +1,55 @@
 
 
 export class Cell{
+	public isWall:boolean = false;
+	public currentLoad:number = 0;
 
-	constructor(x:number, y:number, up:Cell, left:Cell, right:Cell){
+	private up:Cell;
+	private left:Cell;
+	private right:Cell;
+	private down:Cell;
 
+	constructor(
+		public readonly x:number,
+		public readonly y:number,
+		public node:Element
+	){}
+
+	setSiblings(left?:Cell, up?:Cell, right?:Cell, down?:Cell){
+		this.left = left;
+		this.up = up;
+		this.right = right;
+		this.down = down;
+	}
+
+	setFull(){
+		this.currentLoad = 100;
+		this.updateClass();
+	}
+	private updateClass(){
+		if(this.currentLoad >= 100){
+			this.node.className = 'cell high';
+		}
+		else if(this.currentLoad > 60){
+			this.node.className = 'cell med';
+		}
+		else if(this.currentLoad === 0){
+			this.node.className = 'cell';
+		}
+		else{
+			this.node.className = 'cell low';
+		}
+	}
+
+
+	setAsWall(){
+		this.isWall = true;
+		this.node.className = 'cell wall';
+		this.currentLoad = 0;
+	}
+	erase(){
+		this.isWall = false;
+		this.node.className = 'cell';
+		this.currentLoad = 0;
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,79 @@
 import {DrawingTypeSelector} from "./drawing-type-selector";
+import {Cell} from "./cell";
+import {DrawingType} from "./enum/drawing-type.enum";
 
 
 
 //Init
 let drawingSelector = new DrawingTypeSelector();
 drawingSelector.init();
+
+
+let canvas = document.getElementById('canvas');
+canvas.onmousedown = startDrawing;
+canvas.onmouseup = stopDrawing;
+canvas.onmouseleave = stopDrawing;
+
+let cells: Array<Array<Cell>> = [];
+let currentCell:Cell = null;
+
+for(let i = 0; i < 50; i++){
+	let row = [];
+
+	for(let j = 0; j < 50; j++){
+		let node = document.createElement('div');
+		node.className = 'cell';
+		canvas.appendChild(node);
+		let cell = new Cell(i, j, node);
+		node.onmouseover = function(){mouseOver(cell);};
+
+		row.push(cell);
+	}
+
+	cells.push(row);
+}
+
+for(let i = 0; i < 50; i++){
+	for(let j = 0; j < 50; j++){
+		cells[i][j].setSiblings(
+			cells[i-1]?cells[i-1][j]:null,
+			cells[i][j-1]?cells[i][j-1]:null,
+			cells[i+1]?cells[i+1][j]:null,
+			cells[i][j+1]?cells[i][j+1]:null,
+		);
+	}
+}
+
+function mouseOver(cell:Cell){
+	currentCell = cell;
+}
+
+let currentInterval:any = null;
+function startDrawing(){
+	if(currentInterval !== null){
+		return;
+	}
+	execute();
+	currentInterval = setInterval(execute, 10);
+}
+function stopDrawing(){
+	if(currentInterval !== null){
+		clearInterval(currentInterval);
+		currentInterval = null;
+	}
+}
+function execute(){
+	switch (drawingSelector.currentType) {
+		case DrawingType.WATER:
+			currentCell.setFull();
+			break;
+		case DrawingType.WALL:
+			currentCell.setAsWall();
+			break;
+		case DrawingType.DELETE:
+			currentCell.erase();
+			break;
+		default:
+			break;
+	}
+}
